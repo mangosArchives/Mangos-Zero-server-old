35d2650eacc25bf7737c71ec31f0553fe56b17a1
TheLuda <theluda@getmangos.com>
2011-08-31 22:31:17 +0200

    [z1772] Prevent creatues from falling under the map.

--- src/game/GridMap.cpp
+++ src/game/GridMap.cpp
@@ -988,12 +988,12 @@ float TerrainInfo::GetWaterOrGroundLevel(float x, float y, float z, float* pGrou
         // we need ground level (including grid height version) for proper return water level in point
         float ground_z = GetHeight(x, y, z, true, DEFAULT_WATER_SEARCH);
         if (pGround)
-            *pGround = ground_z;
+            *pGround = ground_z + 0.05f;
 
         GridMapLiquidData liquid_status;
 
         GridMapLiquidStatus res = getLiquidStatus(x, y, ground_z, MAP_ALL_LIQUIDS, &liquid_status);
-        return res ? ( swim ? liquid_status.level - 2.0f : liquid_status.level) : ground_z;
+        return res ? ( swim ? liquid_status.level - 2.0f : liquid_status.level) : ground_z + 0.05f;
     }
 
     return VMAP_INVALID_HEIGHT_VALUE;
