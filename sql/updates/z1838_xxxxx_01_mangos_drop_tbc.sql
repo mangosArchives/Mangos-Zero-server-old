49daa44e21561c3c8c42c1bfe1b3a31e8f77438d
TheLuda <theluda@getmangos.com>
2012-09-04 10:40:36 +0200

    [z1838] Dropped TBCisms.
    Signed-off-by: TheLuda <theluda@getmangos.com>

---
+++ sql/updates/z1838_xxxxx_01_mangos_drop_tbc.sql
@@ -0,0 +1,4 @@
+ALTER TABLE db_version CHANGE COLUMN required_z1824_xxxxx_01_mangos_spell_affect_event required_z1838_xxxxx_01_mangos_drop_tbc bit;
+
+DROP TABLE IF EXISTS skill_discovery_template;
+DROP TABLE IF EXISTS skill_extra_item_template;
--- src/game/Level3.cpp
+++ src/game/Level3.cpp
@@ -43,8 +43,6 @@
 #include "Weather.h"
 #include "PointMovementGenerator.h"
 #include "TargetedMovementGenerator.h"
-#include "SkillDiscovery.h"
-#include "SkillExtraItems.h"
 #include "SystemConfig.h"
 #include "Config/Config.h"
 #include "Mail.h"
@@ -148,8 +146,6 @@ bool ChatHandler::HandleReloadAllEventAICommand(char* /*args*/)
 
 bool ChatHandler::HandleReloadAllSpellCommand(char* /*args*/)
 {
-    HandleReloadSkillDiscoveryTemplateCommand((char*)"a");
-    HandleReloadSkillExtraItemTemplateCommand((char*)"a");
     HandleReloadSpellAffectCommand((char*)"a");
     HandleReloadSpellAreaCommand((char*)"a");
     HandleReloadSpellChainCommand((char*)"a");
@@ -476,22 +472,6 @@ bool ChatHandler::HandleReloadReputationSpilloverTemplateCommand(char* /*args*/)
     sLog.outString( "Re-Loading `reputation_spillover_template` Table!" );
     sObjectMgr.LoadReputationSpilloverTemplate();
     SendGlobalSysMessage("DB table `reputation_spillover_template` reloaded.");
-    return true;
-}
-
-bool ChatHandler::HandleReloadSkillDiscoveryTemplateCommand(char* /*args*/)
-{
-    sLog.outString( "Re-Loading Skill Discovery Table..." );
-    LoadSkillDiscoveryTable();
-    SendGlobalSysMessage("DB table `skill_discovery_template` (recipes discovered at crafting) reloaded.");
-    return true;
-}
-
-bool ChatHandler::HandleReloadSkillExtraItemTemplateCommand(char* /*args*/)
-{
-    sLog.outString( "Re-Loading Skill Extra Item Table..." );
-    LoadSkillExtraItemTable();
-    SendGlobalSysMessage("DB table `skill_extra_item_template` (extra item creation when crafting) reloaded.");
     return true;
 }
 
