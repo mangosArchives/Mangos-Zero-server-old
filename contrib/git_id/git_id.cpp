9293a6d864213c80bb1cfebfc86790e97628638e
TheLuda <theluda@getmangos.com>
2012-09-24 21:26:31 +0200

    - Unified file headers.
    Signed-off-by: TheLuda <theluda@getmangos.com>

--- contrib/git_id/git_id.cpp
+++ contrib/git_id/git_id.cpp
@@ -1,6 +1,5 @@
 /*
- * Copyright (C) 2005-2011 MaNGOS <http://getmangos.com/>
- * Copyright (C) 2009-2012 MaNGOSZero <https://github.com/mangos-zero>
+ * Copyright (C) 2005-2012 MaNGOS <http://getmangos.com/>
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
@@ -659,8 +658,7 @@ bool generate_sql_makefile()
     if(!fout) { pclose(cmd_pipe); return false; }
 
     fprintf(fout,
-        "# Copyright (C) 2005-2011 MaNGOS <http://getmangos.com/>\n"
-        "# Copyright (C) 2009-2012 MaNGOSZero <http://github.com/mangos-zero>\n"
+        "# Copyright (C) 2005-2012 MaNGOS <http://getmangos.com/>\n"
         "#\n"
         "# This program is free software; you can redistribute it and/or modify\n"
         "# it under the terms of the GNU General Public License as published by\n"
