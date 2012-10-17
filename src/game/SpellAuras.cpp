bd4a8aa640e9a641b8db043c95966840aae57db4
Cala Haryon <calaftp@free.fr>
2012-10-17 21:20:53 +0200

    Fix spell 24834 (Shadow Bolt Whirl)
    Spell should not stop after 8 ticks
    Spell should rotate clockwise instead of alternating from right to left

--- src/game/SpellAuras.cpp
+++ src/game/SpellAuras.cpp
@@ -934,14 +934,14 @@ void Aura::TriggerSpell()
                     case 24834:                             // Shadow Bolt Whirl
                     {
                         uint32 spellForTick[8] = { 24820, 24821, 24822, 24823, 24835, 24836, 24837, 24838 };
-                        uint32 tick = GetAuraTicks();
+                        uint32 tick = GetAuraTicks() % 8;
                         if (tick < 8)
                         {
                             trigger_spell_id = spellForTick[tick];
 
                             // casted in left/right (but triggered spell have wide forward cone)
                             float forward = target->GetOrientation();
-                            float angle = target->GetOrientation() + (tick % 2 == 0 ? M_PI_F / 2 : - M_PI_F / 2);
+                            float angle = target->GetOrientation() - (tick * 2 * M_PI_F / 8);
                             target->SetOrientation(angle);
                             triggerTarget->CastSpell(triggerTarget, trigger_spell_id, true, NULL, this, casterGUID);
                             target->SetOrientation(forward);
